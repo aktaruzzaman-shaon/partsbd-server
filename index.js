9c7a8fe778f95a50d985d896a33fdb721601cc2c
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2023-03-30 13:03:52 +0600

    Added find operation

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = "mongodb+srv://partsbd:mV1PncQ3MDPiybEX@cluster0.uwk9cua.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const partsCollection = client.db('products').collection("parts");
+
+        app.get('/parts', async (req, res) => {
+            const query = {};
+            const cursor = partsCollection.find(query);
+            const parts = await cursor.toArray();
+            res.send(parts);
+        })
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send("THis is home page");
+})
 
 app.listen(port, () => {
     console.log("Serve is running");
