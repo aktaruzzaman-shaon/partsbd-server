65f50d700eeebb4a4dc6ff9f5f7f221eb401f122
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2023-05-15 21:45:17 +0600

    Null

--- index.js
+++ index.js
@@ -164,10 +164,6 @@ async function run() {
             res.send(result);
         })
 
-        // var ObjectId = require('mongodb').ObjectId;
-        // var id = req.params.gonderi_id;
-        // var o_id = new ObjectId(id);
-        // db.test.find({ _id: o_id })
 
     }
     finally {
